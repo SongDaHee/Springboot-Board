6dba757c9b59da6e4a4fbe247a9c968e44fba86d
DaheeSong <45412032+DaheeSong@users.noreply.github.com>
2018-11-29 17:28:51 +0900

    #3497 springboot 기본파일 적용

---
+++ src/main/java/com/muhayu/dhsongboard/DhsongBoardApplication.java
@@ -0,0 +1,12 @@
+package com.muhayu.dhsongboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhsongBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DhsongBoardApplication.class, args);
+    }
+}
