cdf8b00d2deec2f584c6751238cee268891ab24f
DaheeSong <45412032+DaheeSong@users.noreply.github.com>
2018-12-03 12:19:30 +0900

    #3497 1차 수정

--- src/main/java/com/muhayu/dhsongboard/DhsongBoardApplication.java
+++ src/main/java/com/muhayu/dhsongboard/DhsongBoardApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DhsongBoardApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(DhsongBoardApplication.class, args);
     }
 }
