3ee67bc2ac9ccb7737c921c2cad9bb4f150be6e2
DaheeSong <45412032+DaheeSong@users.noreply.github.com>
2018-12-21 09:27:28 +0900

    소스정리중

--- src/main/java/com/muhayu/dhsongboard/service/UserinfoService.java
+++
@@ -1,8 +0,0 @@
-package com.muhayu.dhsongboard.service;
-
-import com.muhayu.dhsongboard.model.Userinfomodel;
-
-public interface UserinfoService {
-
-    int userinfoInsert(Userinfomodel userinfomodel);
-}
